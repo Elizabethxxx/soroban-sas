be0290dc8ce5112fd4c3725cafab56f3f8c2a58f
geni3 <julietaallendorf@gmail.com>
2026-07-02 15:09:41 +0100

    feat(indexer): create mapping of recipient_address -> Vec<UID>

--- contracts/indexer/src/lib.rs
+++ contracts/indexer/src/lib.rs
@@ -12,6 +12,9 @@ impl Indexer {
     }
 
     pub fn index_attestation(_env: Env, _uid: UID, _recipient: Address, _schema_uid: UID) {
-        // Hook to receive new attestations from core SAS
+        // Recipient -> Vec<UID>
+        let mut recipient_uids: soroban_sdk::Vec<UID> = _env.storage().persistent().get(&_recipient).unwrap_or_else(|| soroban_sdk::Vec::new(&_env));
+        recipient_uids.push_back(_uid.clone());
+        _env.storage().persistent().set(&_recipient, &recipient_uids);
     }
 }
