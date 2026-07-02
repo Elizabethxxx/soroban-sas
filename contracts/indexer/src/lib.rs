65fe049865113e3824ebf22a2e5c80312bcba615
mode <sawingmowthe@gmail.com>
2026-07-02 12:22:49 +0100

    feat(indexer): implement hook to receive new UIDs from core SAS

--- contracts/indexer/src/lib.rs
+++ contracts/indexer/src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, Env};
+use soroban_sdk::{contract, contractimpl, Address, Env};
+use soroban_sas_common::UID;
 
 #[contract]
 pub struct Indexer;
@@ -8,5 +9,9 @@ pub struct Indexer;
 impl Indexer {
     pub fn init(_env: Env) {
         // Initialize reverse lookup
+    }
+
+    pub fn index_attestation(_env: Env, _uid: UID, _recipient: Address, _schema_uid: UID) {
+        // Hook to receive new attestations from core SAS
     }
 }
