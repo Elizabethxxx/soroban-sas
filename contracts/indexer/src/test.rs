7e2ff075685b3700bda310f7329795ccfba57602
geni3 <julietaallendorf@gmail.com>
2026-07-07 17:41:09 +0100

    test(indexer): query schemas and recipients to verify reverse lookup

--- contracts/indexer/src/test.rs
+++ contracts/indexer/src/test.rs
@@ -33,3 +33,25 @@ fn test_chunked_storage_limits() {
         client.index_attestation(&uid, &recipient, &schema_uid);
     }
 }
+
+#[test]
+fn test_reverse_lookup() {
+    let env = Env::default();
+    let indexer_id = env.register_contract(None, Indexer);
+    let client = IndexerClient::new(&env, &indexer_id);
+    
+    let schema_uid = UID([4u8; 32]);
+    let recipient = Address::generate(&env);
+    
+    let uid1 = UID([10u8; 32]);
+    let uid2 = UID([11u8; 32]);
+    
+    client.index_attestation(&uid1, &recipient, &schema_uid);
+    client.index_attestation(&uid2, &recipient, &schema_uid);
+    
+    let recipient_uids = client.get_attestations_by_recipient(&recipient);
+    assert_eq!(recipient_uids.len(), 2);
+    
+    let schema_uids = client.get_attestations_by_schema(&schema_uid);
+    assert_eq!(schema_uids.len(), 2);
+}
